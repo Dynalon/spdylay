baf5311143a4a35b009036f423089369aea20e80
Timo Dörr <timo@latecrew.de>
2013-01-18 13:28:32 +0100

    Updated bench script with modular version

--- src/bench.sh
+++ src/bench.sh
@@ -1,85 +1,72 @@
 #!/bin/bash
-
+#set -e
+#set -x
+VERBOSE=1
 SPDYCAT="./spdycat"
 SPDYCAT_BASE_PARAM="-v -3 -n"
 
-NUM_RUNS=50
-HALF_RUNS=`echo $NUM_RUNS | awk '{ half = $1/2; print half}'`
+NUM_RUNS=4
+#50
 SSH_REMOTE="tb15"
+SPDY_REMOTE="10.0.0.15:8080"
+CWD="/home/s_doerr/spdylay/src"
+RCWD=$CWD
+BASE_LOGDIR="$CWD/logs/"
+SNIFF_DEVICE="eth0"
+SNIFF_DEVICE_REMOTE="eth0"
 
-REMOTE_PATH="/home/s_doerr/spdylay/src/"
+HALF_RUNS=`echo $NUM_RUNS | awk '{ half = $1/2; print half}'`
 
-# before the first run, checkout the git source and rebuild
-echo "## UPDATE LOCAL SOURCE TO LATEST GIT REV";
-pushd '../'
-git pull > /dev/null
-echo "## BUILDING LOCAL SOURCE";
-make > /dev/null
-popd
+STDOUT="/dev/null"
 
-# do the same on the remote side
-echo "## UPDATE AND BUILD REMOTE SOURCE TO LATEST GIT REV";
-ssh $SSH_REMOTE 'cd /home/s_doerr/spdylay/; git pull; make' > /dev/null
+source bench_functions.sh
 
-echo "## LOCAL AND REMOTE REPO UP TO DATE"
-
-# delete any previous collected log data
-echo "## DELETING PREVIOUS COLLECTED LOG DATA"
-rm -rf logs/
-mkdir logs
-ssh -t $SSH_REMOTE "cd /home/s_doerr/spdylay/src/; rm -rf logs; mkdir logs"
-
-echo "## START BENCHMARK WITH $NUM_RUNS RUNS"
+update_from_git
+delete_previous_logs
 
 function do_benchmark {
 
 	WEBSITE="$1"
-	URL="https://10.0.0.15:8080/$WEBSITE/index.html"
+	URL="https://$SPDY_REMOTE/$WEBSITE/index.html"
 	if [ "$2" == "push" ]; then
-		echo "[PUSH] STARTING $WEBSITE BENCHMARK"
-		SPDYD_CMD="cd /home/s_doerr/spdylay/src/; screen -m -d -S spdyd ./start_server.sh -a --no-nagle > logs/server.log; sleep 2"
+		SPDYD_CMD="./start_server.sh -a"
 		SPDYCAT_ARGS=""
 		LOGNAME="$WEBSITE-push"
+		print_header "[PUSH] STARTING $WEBSITE BENCHMARK"
 	else
-		echo "[FETCH] STARTING $WEBSITE BENCHMARK"
-		SPDYD_CMD="cd /home/s_doerr/spdylay/src/; screen -m -d -S spdyd ./start_server.sh --no-nagle > logs/server.log; sleep 2"
+		print_msg "[FETCH] STARTING $WEBSITE BENCHMARK"
+		SPDYD_CMD="./start_server.sh"
 		SPDYCAT_ARGS="-p"
 		LOGNAME="$WEBSITE-fetch"
 	fi
 
-	# start the server on the remote end via screen
-	ssh -t $SSH_REMOTE $SPDYD_CMD 
-	
-	# perform two request to warm start
-	$SPDYCAT $SPDYCAT_BASE_PARM $SPDYCAT_ARGS $URL > /dev/null
-	$SPDYCAT $SPDYCAT_BASE_PARM $SPDYCAT_ARGS $URL > /dev/null
+	TCPDUMP_CLIENT="dumpcap -q -i $SNIFF_DEVICE -w logs/$LOGNAME.client.pcap"
+	TCPDUMP_SERVER="dumpcap -q -i $SNIFF_DEVICE_REMOTE -w logs/$LOGNAME.server.pcap"
 
-	sleep 2
-	# for the control log, sniff data via tcpdump
-	#TCPDUMP_CLIENT="tcpdump -s 0 -i eth0 -w logs/$LOGNAME.client.pcap"
-	TCPDUMP_CLIENT="dumpcap -q -i br0 -w logs/$LOGNAME.client.pcap"
-	#TCPDUMP_SERVER="tcpdump -s 0 -i eth0 -w logs/server.pcap"
-	echo $TCPDUMP_SERVER
-	# client sniffing
-	screen -m -d -S sniffer $TCPDUMP_CLIENT 
-	# server sniffing
-	#ssh -t $SSH_REMOTE "cd /home/s_doerr/spdylay/src/; screen -d -S tcpdump -m $TCPDUMP_SERVER"
-	ssh -t $SSH_REMOTE "cd /home/s_doerr/spdylay/src/; screen -m -d -S sniffer dumpcap -q -i br0 -w logs/server.pcap; sleep 2"
+	print_msg "Starting spdyd on the server"
+	ERR=$(execute_remote_bg "$SPDYD_CMD" "spdyd" "logs/$LOGNAME.server.log")
+
+	print_msg "Sending two requests for warm start"
+	$SPDYCAT $SPDYCAT_BASE_PARAM $SPDYCAT_ARGS $URL > $STDOUT
+	$SPDYCAT $SPDYCAT_BASE_PARAM $SPDYCAT_ARGS $URL > $STDOUT
+
+	print_msg "starting network sniffer on the client"	
+	execute_bg "$TCPDUMP_CLIENT" "tcpdump_client"
+
+	print_msg "starting network sniffer on the server"	
+	execute_remote_bg "$TCPDUMP_SERVER" "tcpdump_server"
 
 	# one control call with verbose output that we can examine
 	$SPDYCAT $SPDYCAT_BASE_PARAM $SPDYCAT_ARGS -v $URL > logs/$LOGNAME.client.log
-
-	sleep 1
+	
 	# end the sniffing for the control log
 	# client
-	killall -TERM dumpcap
-	ssh -t $SSH_REMOTE "killall -TERM dumpcap; sleep 1; screen -S sniffer -X quit; sleep 2"
-	screen -S sniffer -X quit 
+	terminate_bg "tcpdump_client"
+	terminate_remote_bg "tcpdump_server"
 
 	# copy the logs from the server to the client in the background
-	scp tb15:/home/s_doerr/spdylay/src/logs/server.pcap /home/s_doerr/spdylay/src/logs/$LOGNAME.server.pcap &
-	scp tb15:/home/s_doerr/spdylay/src/logs/server.log /home/s_doerr/spdylay/src/logs/$LOGNAME.server.log &
-
+	scp tb15:/home/s_doerr/spdylay/src/logs/$LOGNAME.server.pcap /home/s_doerr/spdylay/src/logs/$LOGNAME.server.pcap > $STDOUT
+	#scp tb15:/home/s_doerr/spdylay/src/logs/$LOGNAME.server.log /home/s_doerr/spdylay/src/logs/$LOGNAME.server.log 
 
 	# the actual benchmarking runs
 	for i in $(seq 1 1 $NUM_RUNS)
@@ -89,7 +76,7 @@ function do_benchmark {
 	done
 
 	# kill the screen session (and thus the server) on the remote side
-	ssh $SSH_REMOTE 'screen -S spdyd -X quit; sleep 2;' > /dev/null
+	terminate_remote_bg "spdyd"
 
 	# concatenate all runs into large output
 	cat logs/$LOGNAME.run*.log > logs/$LOGNAME.full.log
@@ -99,16 +86,16 @@ function do_benchmark {
 	MAX=`cat logs/$LOGNAME.full.log|grep Total|sort|tail -n 1|cut -f2 -d':'`
 	AVG=`cat logs/$LOGNAME.full.log|grep Total|awk '{sum+=$4} END { print sum/NR "ms"}'`
 
-	echo -e "[RESULT $LOGNAME] Transfer times:\tMIN: ${MIN}\tMAX: ${MAX}\tMED: ${MEDIAN}\tAVG: ${AVG}"
+	print_result "[RESULT $LOGNAME] Transfer times:\tMIN: ${MIN}\tMAX: ${MAX}\tMED: ${MEDIAN}\tAVG: ${AVG}"
 }
-
 
 for site in $@ 
 do
 	do_benchmark $site push
 	do_benchmark $site fetch
 done
-	
+
+print_all_results	
 # copy the whole logs folder to i08
-ssh -t s_drr@i08fs1.ira.uka.de "rm -rf /home/s_drr/logs/"
-scp -r logs s_drr@i08fs1.ira.uka.de:
+#ssh -t s_drr@i08fs1.ira.uka.de "rm -rf /home/s_drr/logs/"
+#scp -r logs s_drr@i08fs1.ira.uka.de:
---
+++ src/bench_functions.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+ALL_RESULTS=""
+function print_msg() 
+{
+	if [ $VERBOSE ]; then
+		echo -e "# $1"
+	fi
+}
+function print_header()
+{
+	if [ $VERBOSE ]; then
+		echo -e "\n\e[01;36m## $1\e[00m"
+	fi
+}
+function print_result ()
+{
+	ALL_RESULTS="${ALL_RESULTS}\n$1"
+	echo -e "\n\e[0;35m## $1\e[00m"
+}
+function print_all_results ()
+{
+	echo -e "\n\e[1;32mRESULTS:\n ${ALL_RESULTS}\e[00m"
+}
+function error()
+{
+	echo -e "\n\e[00;31m\t$1\n\e[00m" >&2 
+	exit 1
+}
+
+# returns the stdout of the remote command
+function execute_remote ()
+{
+	local _CMD=$1
+		
+	if [ $2 ]; then
+		local _REMOTE=$2
+	else
+		local _REMOTE=$SSH_REMOTE
+	fi	
+	
+	if [ $3 ]; then
+		local _RCWD=$2
+	else
+		local _RCWD=$RCWD
+	fi
+
+	# execute the command remotely
+	_RSTDOUT=$(ssh $_REMOTE "cd $_RCWD; $_CMD; sleep 0.3; exit;")
+
+	if [ $? -ne 0 ]; then
+		error "Failure on execute_remote($_CMD $_REMOTE $_RCWD)"
+	else
+		sleep 1
+		#echo $_RSTDOUT
+	fi
+}
+function execute_bg()
+{
+	local _CMD=$1
+	local _HANDLE=$2
+	
+	if [ $3 ]; then
+		local _OUTPUT=$3
+	else
+		local _OUTPUT="/dev/null"
+	fi	
+
+	_LCMD="screen -m -d -S $_HANDLE $_CMD > $_OUTPUT"
+	_STDOUT=`eval ${_LCMD}`
+	
+	sleep 1
+	#echo $_STDOUT
+}
+
+function terminate_bg()
+{
+	local _HANDLE=$1
+	_SESSION=$(screen -ls $_HANDLE | grep $_HANDLE | cut -f2)
+	_STDOUT=$(screen -S $_SESSION -X quit)
+}
+
+function execute_remote_bg()
+{
+	local _CMD=$1
+	local _HANDLE=$2
+
+	if [ $3 ]; then
+		local _OUTPUT=$3
+	else
+		local _OUTPUT="/dev/null"
+	fi	
+
+	if [ $4 ]; then
+		local _REMOTE=$4
+	else
+		local _REMOTE=$SSH_REMOTE
+	fi	
+	
+	if [ $5 ]; then
+		local _RCWD=$5
+	else
+		local _RCWD=$RCWD
+	fi
+
+	# execute the command remotely
+	
+	_LCMD="ssh $_REMOTE 'cd $_RCWD; screen -m -d -S $_HANDLE $_CMD'"
+	_RSTDOUT=`eval ${_LCMD}`
+
+	if [ $? -ne 0 ]; then
+		error "Failure on execute_remote($_CMD $_REMOTE $_HANDLE)"
+	else
+		sleep 1
+	#	echo $_RSTDOUT
+	fi
+}
+function terminate_remote_bg()
+{
+	local _HANDLE=$1
+
+	if [ $2 ]; then
+		local _REMOTE=$2
+	else
+		local _REMOTE=$SSH_REMOTE
+	fi
+	
+	_LCMD="ssh -t $_REMOTE screen -ls $_HANDLE | grep $_HANDLE | cut -f2" 
+	_SESSION=`eval ${_LCMD}`
+
+	_LCMD="screen -S $_SESSION -X quit"
+	execute_remote "$_LCMD"
+	#echo $_RESULT
+}
+
+function update_from_git ()
+{
+	print_header "UPDATE SOURCES TO LATEST GIT REV"
+	pushd '../' > $STDOUT
+	git pull > $STDOUT
+	print_msg "building local source"
+	make > $STDOUT
+	popd > $STDOUT
+	# do the same on the remote side
+	print_msg "update and build remote source on $SSH_REMOTE"
+	# will fail lots of times if i have untracked changes...
+	ERR=`execute_remote 'git pull 2> /dev/null; make '` || exit $?
+	print_msg "local and remote source up to date"
+}
+
+function delete_previous_logs ()
+{
+	print_header "DELETING PREVIOUS COLLECTED LOG DATA"
+	print_msg "deleting local logs"
+	rm -rf $BASE_LOGDIR
+	mkdir $BASE_LOGDIR
+	print_msg "deleting logs on remote $SSH_REMOTE"
+	execute_remote "rm -rf logs; mkdir logs"
+}
